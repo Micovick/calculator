7d477c43f81dce5a1879b7bc0b4e37fcd69e87f3
Euclides Cardoso Júnior <euclides.c.jr@gmail.com>
2023-09-21 22:08:17 -0300

    Initial project files

---
+++ src/test/java/io/loanpro/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.loanpro.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
